7cee85ddd7fa121b62ecc6658da623bdcf97033f
karinakozarova <karinakozarova1303@gmail.com>
2017-10-26 18:06:10 +0300

    migrate db

--- privnote/app/controllers/messages_controller.rb
+++ privnote/app/controllers/messages_controller.rb
@@ -1,4 +1,13 @@
 class MessagesController < ApplicationController
+	puts "HEre"
 	class Product < ApplicationRecord
+		self.table_name = "testing"
+		user = User.new do |u|
+  			u.name = "David"
+  			u.occupation = "Code Artist"
+		end
+		users = User.all
+		users.save!
+		puts users
 	end
 end
