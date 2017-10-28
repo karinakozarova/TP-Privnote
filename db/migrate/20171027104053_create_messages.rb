93d64e4fccd0926554cfae0dfa9b01c1ebfe15fa
karinakozarova <karinakozarova1303@gmail.com>
2017-10-28 22:25:14 +0300

    upd

--- db/migrate/20171027104053_create_messages.rb
+++ db/migrate/20171027104053_create_messages.rb
@@ -1,8 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :messages do |t|
-      t.text :text # add info  to database
+      t.text :text
       t.timestamps
     end
   end
-e n
+end
